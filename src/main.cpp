3e33c6bc6f4096acc76ca1d38f4be0066c61a7a0
cdent-13 <147329255+AlgonquinCamper@users.noreply.github.com>
2024-10-22 16:42:12 -0400

    Chagnes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <Adafruit_AHTX0.h>
 #include <TM1637Display.h>
+#include <wifi.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
 
 #define CLK D6
 #define DIO D5
@@ -17,7 +21,10 @@
 
 TM1637Display display(CLK, DIO);
 Adafruit_AHTX0 aht;
-bool showTemp;
+WiFiUDP ntpUDP;
+const long offSet = -4 * 60 * 60;
+NTPClient timeClient(ntpUDP, "0.ca.pool.ntp.org", offSet);
+
 bool changed;
 int lastState = 1;
 int displayOption;
@@ -34,59 +41,107 @@ void showTemp(sensors_event_t temp){
 }
 
 void showHumidity(sensors_event_t humidity){
-  if (changed){
+  if(changed) {
     display.clear();
     changed = false;
   }
   display.showNumberDecEx(humidity.relative_humidity, 0x40, false, 2, 0);
 }
+// I'm sorry this doesnt follow convetions, I thought it was a great pun...one might say, punny...
+void itsSHOWTIME(String time){
+  Serial.println(time);
+}
+
+void cycle(sensors_event_t humidity, sensors_event_t temp){
+  display.clear();
+  itsSHOWTIME(timeClient.getFormattedTime());
+  delay(2000);
+  display.clear();
+  changed = true;
+
+  showTemp(temp);
+  delay(2000);
+  display.clear();
+  changed = true;
+
+  showHumidity(humidity);
+  delay(2000);
+  display.clear();
+  changed = true;
+
+}
+
+void showDashes() {
+  
+}
 
 void setup() {
   displayOption = 0;
   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    display.clear();
+    delay(500);
+    display.setSegments((const uint8_t[]){0x40, 0x40, 0x40, 0x40});
+    delay(500);
+  }
+
+  timeClient.begin();
+  
+
   if (!aht.begin()) {
-  while (1) delay(10);
+    Serial.println("Cannot find AHT");
+    while (1) delay(10);
   }
   // Clear display of any remnants
   display.clear();
-  // Set the degrees symbol
-  display.setSegments((const uint8_t[]){0x00}, 1, 1);
-  display.setSegments((const uint8_t[]){0x63}, 2, 3);
-  display.setBrightness(0x0a); // set brightness
-  // Set pin mode for button
+  display.setBrightness(0x0a); 
   pinMode(D7, INPUT_PULLUP);
 }
 
 void loop() {
+  timeClient.update();
   // checks to see if the button is constant press
   if (digitalRead(D7) == 1){  
     lastState = digitalRead(D7);
   }
 // sensors_event_t is a C struct
   sensors_event_t humidity, temp;
-// populate temp and humidity objects with fresh data
   aht.getEvent(&humidity, &temp);
-// print temp
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature); Serial.println(" degrees C");
-// print humidity
-  Serial.print("Humidity: ");
-  Serial.print(humidity.relative_humidity); Serial.println("% rH");
 
-  // If the show temp variable is set to true, show the temp and if not, show the humidity
-  if(showTemp){
+  switch (displayOption)
+  {
+    case 0:
+      itsSHOWTIME(timeClient.getFormattedTime());
+      break;
   
-  }
-  else{
-   
+    case 1:
+      showHumidity(humidity);
+      break;
 
+    case 2:
+      showTemp(temp);
+      break;
+
+    case 3:
+      cycle(humidity, temp);
+      break;
   }
 
-  // Checks to see if the button was pressed, and stops constant switching with button press
   if (digitalRead(D7) == 0 && digitalRead(D7) != lastState){
-    changed = true;
-    showTemp = !showTemp;
     lastState = digitalRead(D7);
+    changed = true;
+    if (displayOption == 3){
+      displayOption = 0;
+      Serial.println(String(displayOption));
+    }
+    else{
+      displayOption++;
+      Serial.println(String(displayOption));
+    }
   }
   delay(50);
 }
---
+++ src/wifi.h
@@ -0,0 +1,2 @@
+const char* ssid = "iPhone";
+const char* password = "Hunter123.";
